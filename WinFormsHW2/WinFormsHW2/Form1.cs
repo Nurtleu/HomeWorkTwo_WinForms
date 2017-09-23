e466acae42b95c3e3a759e7b15ddb7782f3d661f
Nurtleu <nur71701@gmail.com>
2017-09-23 13:48:05 +0600

    Home Work2

--- WinFormsHW2/WinFormsHW2/Form1.cs
+++ WinFormsHW2/WinFormsHW2/Form1.cs
@@ -48,5 +48,10 @@ namespace WinFormsHW2
                 return;
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
